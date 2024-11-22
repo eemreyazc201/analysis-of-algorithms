701798462c52066ad4018dbadf2b0b480c4cf4e3
Emre YAZICI <eemreyazc@hotmail.com>
2024-11-22 23:01:19 +0300

    Bubble Sort is coded.

--- twitter-analysis/src/sort.cpp
+++ twitter-analysis/src/sort.cpp
@@ -1,6 +1,76 @@
 #include "tweet.h"
 
-void bubbleSort (std::vector<Tweet>& tweets, const std::string& sortBy, bool ascending) {}
+void bubbleSort (std::vector<Tweet>& tweets, const std::string& sortBy, bool ascending) {
+    int size = tweets.size(); 
+
+    if (ascending && sortBy == "tweetID") {
+        for (int i = 0; i < size - 1; i++) {
+            bool swapped = false;
+            for (int j = 0; j < size - i - 1; j++) {
+                if (tweets[j].tweetID > tweets[j + 1].tweetID) {
+                    Tweet temp = tweets[j]; tweets[j] = tweets[j + 1]; tweets[j + 1] = temp; swapped = true;
+                }
+            }
+
+            if (!swapped) {break;}
+        }
+    } else if (ascending && sortBy == "retweetCount") {
+        for (int i = 0; i < size - 1; i++) {
+            bool swapped = false;
+            for (int j = 0; j < size - i - 1; j++) {
+                if (tweets[j].retweetCount > tweets[j + 1].retweetCount) {
+                    Tweet temp = tweets[j]; tweets[j] = tweets[j + 1]; tweets[j + 1] = temp; swapped = true;
+                }
+            }
+
+            if (!swapped) {break;}
+        }        
+    } else if (ascending && sortBy == "favoriteCount") {
+        for (int i = 0; i < size - 1; i++) {
+            bool swapped = false;
+            for (int j = 0; j < size - i - 1; j++) {
+                if (tweets[j].favoriteCount > tweets[j + 1].favoriteCount) {
+                    Tweet temp = tweets[j]; tweets[j] = tweets[j + 1]; tweets[j + 1] = temp; swapped = true;
+                }
+            }
+
+            if (!swapped) {break;}
+        }        
+    } else if (!ascending && sortBy == "tweetID") {
+        for (int i = 0; i < size - 1; i++) {
+            bool swapped = false;
+            for (int j = 0; j < size - i - 1; j++) {
+                if (tweets[j].tweetID < tweets[j + 1].tweetID) {
+                    Tweet temp = tweets[j]; tweets[j] = tweets[j + 1]; tweets[j + 1] = temp; swapped = true;
+                }
+            }
+
+            if (!swapped) {break;}
+        }
+    } else if (!ascending && sortBy == "retweetCount") {
+        for (int i = 0; i < size - 1; i++) {
+            bool swapped = false;
+            for (int j = 0; j < size - i - 1; j++) {
+                if (tweets[j].retweetCount < tweets[j + 1].retweetCount) {
+                    Tweet temp = tweets[j]; tweets[j] = tweets[j + 1]; tweets[j + 1] = temp; swapped = true;
+                }
+            }
+
+            if (!swapped) {break;}
+        }        
+    } else if (!ascending && sortBy == "favoriteCount") {
+        for (int i = 0; i < size - 1; i++) {
+            bool swapped = false;
+            for (int j = 0; j < size - i - 1; j++) {
+                if (tweets[j].favoriteCount < tweets[j + 1].favoriteCount) {
+                    Tweet temp = tweets[j]; tweets[j] = tweets[j + 1]; tweets[j + 1] = temp; swapped = true;
+                }
+            }
+
+            if (!swapped) {break;}
+        }        
+    }
+}
 
 void insertionSort (std::vector<Tweet>& tweets, const std::string& sortBy, bool ascending) {}
 
