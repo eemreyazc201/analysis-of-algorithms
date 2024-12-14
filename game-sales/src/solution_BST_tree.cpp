d76176e33d7dc15be0dcede687687e0a2a5a557f
Emre YAZICI <eemreyazc@hotmail.com>
2024-12-14 18:17:47 +0300

    HW template is updated.

--- game-sales/src/solution_BST_tree.cpp
+++ game-sales/src/solution_BST_tree.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <iomanip>
 #include <string.h>
+#include <string>
 #include <fstream>
 #include <vector>
 #include <algorithm>
@@ -42,9 +43,24 @@ class BST_tree {
         Node* get_root () {return this->root;}
 
 		Node* BST_insert (Node* root, Node* ptr) {
-			
-		    // Fill this function.
-			
+			if (root == NULL) {
+				if (best_seller[0] == NULL || ptr->key.na_sales > best_seller[0]->na_sales) {best_seller[0] = &ptr->key;}
+				if (best_seller[1] == NULL || ptr->key.eu_sales > best_seller[1]->eu_sales) {best_seller[1] = &ptr->key;}
+				if (best_seller[2] == NULL || ptr->key.others_sales > best_seller[2]->others_sales) {best_seller[2] = &ptr->key;}
+				return ptr;
+			}
+
+			if (compareAlphabetically(ptr->key, root->key) == SMALLER) {
+				root->left = BST_insert(root->left, ptr); root->left->parent = root;
+			} else if (compareAlphabetically(ptr->key, root->key) == BIGGER) {
+				root->right = BST_insert(root->right, ptr); root->right->parent = root;
+			} else if (compareAlphabetically(ptr->key, root->key) == EQUAL) {
+				root->key.na_sales += ptr->key.na_sales; root->key.eu_sales += ptr->key.eu_sales; root->key.others_sales += ptr->key.others_sales;
+				if (best_seller[0] == NULL || root->key.na_sales > best_seller[0]->na_sales) {best_seller[0] = &root->key;}
+				if (best_seller[1] == NULL || root->key.eu_sales > best_seller[1]->eu_sales) {best_seller[1] = &root->key;}
+				if (best_seller[2] == NULL || root->key.others_sales > best_seller[2]->others_sales) {best_seller[2] = &root->key;}
+			}
+
 			return root;
 		}
 
@@ -70,9 +86,23 @@ void print_best_sellers (int year, publisher* temp_publisher[3]) {
 }
 
 BST_tree generate_BST_tree_from_csv (string file_name) {
-    BST_tree temp_BSTtree;
+    BST_tree temp_BSTtree; 
+	
+	
+	
+	vector<publisher> publishers; ifstream file (file_name); if (file.is_open()) {
+		string line; getline(file, line); while (getline(file, line)) {
+			stringstream ss (line); string name, platform, year, publisherName; float na_sales, eu_sales, others_sales;
+			ss >> name; ss.ignore(1, ','); ss >> platform; ss.ignore(1, ','); ss >> year; ss.ignore(1, ','); ss >> publisherName; ss.ignore(1, ','); ss >> na_sales; ss.ignore(1, ','); ss >> eu_sales; ss.ignore(1, ','); ss >> others_sales;
+			
+			
+			
+			
+			
 
-    // Fill this function.
+		} file.close();
+	} else {cerr << "Error: Unable to open file " << file_name << endl;
+	}
 
     return temp_BSTtree;
 }
@@ -83,4 +113,15 @@ int main (int argc, char* argv[]) {
     // Fill this function.
 
 	return EXIT_SUCCESS;
+}
+
+enum Alphabetic_Order {BIGGER, EQUAL, SMALLER};
+Alphabetic_Order compareAlphabetically (publisher& a, publisher& b) {
+	int size = min(a.name.size(), b.name.size()); for (int i = 0; i < size; i++) {
+		char ai = (char)tolower(a.name[i]); char bi = (char)tolower(b.name[i]);
+		if (ai == bi) {continue;} 
+		else {return (ai < bi) ? SMALLER : BIGGER;}
+	}
+
+	return (a.name.size() == b.name.size()) ? EQUAL : ((a.name.size() < b.name.size()) ? SMALLER : BIGGER);
 }
--- game-sales/src/solution_RBT_tree.cpp
+++ game-sales/src/solution_RBT_tree.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <iomanip>
 #include <string.h>
+#include <string>
 #include <fstream>
 #include <vector>
 #include <algorithm>
