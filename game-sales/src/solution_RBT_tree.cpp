c9c5bc3ec5c1ab9e4f80aa9c9f409d7361d08b30
Emre YAZICI <eemreyazc@hotmail.com>
2024-12-16 23:50:52 +0300

    Should be fixed.

--- game-sales/src/solution_RBT_tree.cpp
+++ game-sales/src/solution_RBT_tree.cpp
@@ -55,30 +55,31 @@ class RB_tree {
 
         Node* get_root () {return this->root;}
 
-		Node* RB_insert (Node* root, Node*& ptr) {
-			Node* parent = NULL; Node* temp = root;
+		void insertValue (vector<string> n) {
+		    publisher temp; temp.name = n[3]; temp.na_sales = stof(n[4]); temp.eu_sales = stof(n[5]); temp.others_sales = stof(n[6]);
+			Node* ptr = new Node(temp); root = RB_insert(root, ptr);
+		}
 
-			while (temp != NULL) {
-				parent = temp;
+		Node* RB_insert (Node* root, Node* ptr) {
+			preorder();
+			Node* parent = NULL; Node* curr = root; while (curr != NULL) {
+				parent = curr; 
+				
+				if (compareAlphabetically(ptr->key, curr->key) == SMALLER) {curr = curr->left;}
+				else if (compareAlphabetically(ptr->key, curr->key) == BIGGER) {curr = curr->right;}
+				else if (compareAlphabetically(ptr->key, curr->key) == EQUAL) {
+					curr->key.na_sales += ptr->key.na_sales; curr->key.eu_sales += ptr->key.eu_sales; curr->key.others_sales += ptr->key.others_sales;
 
-				if (compareAlphabetically(ptr->key, temp->key) == SMALLER) {temp = temp->left;}
-				else if (compareAlphabetically(ptr->key, temp->key) == BIGGER) {temp = temp->right;}
-				else if (compareAlphabetically(ptr->key, temp->key) == EQUAL) {
-					temp->key.na_sales += ptr->key.na_sales; temp->key.eu_sales += ptr->key.eu_sales; temp->key.others_sales += ptr->key.others_sales;
-					
-					if (best_seller[0] == NULL || temp->key.na_sales > best_seller[0]->na_sales) {best_seller[0] = &temp->key;}
-					if (best_seller[1] == NULL || temp->key.eu_sales > best_seller[1]->eu_sales) {best_seller[1] = &temp->key;}
-					if (best_seller[2] == NULL || temp->key.others_sales > best_seller[2]->others_sales) {best_seller[2] = &temp->key;}
-
-					ptr = temp;
+					if (best_seller[0] == NULL || curr->key.na_sales > best_seller[0]->na_sales) {best_seller[0] = &curr->key;}
+					if (best_seller[1] == NULL || curr->key.eu_sales > best_seller[1]->eu_sales) {best_seller[1] = &curr->key;}
+					if (best_seller[2] == NULL || curr->key.others_sales > best_seller[2]->others_sales) {best_seller[2] = &curr->key;}
 
 					return root;
 				}
 			}
 
 			ptr->parent = parent;
-
-			if (parent == NULL) {root = ptr;}
+			if (parent == NULL) {root = ptr; this->root = ptr;} // here root is local variable
 			else if (compareAlphabetically(ptr->key, parent->key) == SMALLER) {parent->left = ptr;} 
 			else {parent->right = ptr;}
 
@@ -86,29 +87,10 @@ class RB_tree {
 			if (best_seller[1] == NULL || ptr->key.eu_sales > best_seller[1]->eu_sales) {best_seller[1] = &ptr->key;}
 			if (best_seller[2] == NULL || ptr->key.others_sales > best_seller[2]->others_sales) {best_seller[2] = &ptr->key;}
 
+			RB_insert_fixup(ptr);
+
 			return root;
 		}
-
-		void insertValue (vector<string> n) {
-		    publisher temp; temp.name = n[3]; temp.na_sales = stof(n[4]); temp.eu_sales = stof(n[5]); temp.others_sales = stof(n[6]);
-			Node* ptr = new Node(temp); root = RB_insert(root, ptr); RB_insert_fixup(ptr);
-		}
-
-        void RB_left_rotate (Node *ptr) {
-			Node* child = ptr->right; ptr->right = child->left;
-			if (child->left != NULL) {child->left->parent = ptr;}
-			child->parent = ptr->parent;
-			if (ptr->parent == NULL) {root = child;} else if (ptr == ptr->parent->left) {ptr->parent->left = child;} else {ptr->parent->right = child;}
-			child->left = ptr; ptr->parent = child;
-        }
-
-        void RB_right_rotate (Node *ptr) {
-			Node* child = ptr->left; ptr->left = child->right;
-			if (child->right != NULL) {child->right->parent = ptr;}
-			child->parent = ptr->parent;
-			if (ptr->parent == NULL) {root = child;} else if (ptr == ptr->parent->right) {ptr->parent->right = child;} else {ptr->parent->left = child;}
-			child->right = ptr; ptr->parent = child;
-        }
 
         void RB_insert_fixup (Node *ptr) {
 			Node* parent = NULL; Node* grandParent = NULL;
@@ -120,7 +102,7 @@ class RB_tree {
 						grandParent->set_color(1); parent->set_color(0); uncle->set_color(0); ptr = grandParent;
 					} else {
 						if (ptr == parent->right) {RB_left_rotate(parent); ptr = parent; parent = ptr->parent;}
-						RB_right_rotate(grandParent); int temp = parent->color; parent->color = grandParent->color; grandParent->color = temp; ptr = parent;
+						RB_right_rotate(grandParent); int temp = parent->get_color(); parent->set_color(grandParent->get_color()); grandParent->set_color(temp); ptr = parent;
 					}
 				} else {
 					Node* uncle = grandParent->left;
@@ -128,29 +110,47 @@ class RB_tree {
 						grandParent->set_color(1); parent->set_color(0); uncle->set_color(0); ptr = grandParent;
 					} else {
 						if (ptr == parent->left) {RB_right_rotate(parent); ptr = parent; parent = ptr->parent;}
-						RB_left_rotate(grandParent); int temp = parent->color; parent->color = grandParent->color; grandParent->color = temp; ptr = parent;
+						RB_left_rotate(grandParent); int temp = parent->get_color(); parent->set_color(grandParent->get_color()); grandParent->set_color(temp); ptr = parent;
 					}
 				}
-			} root->set_color(0);   
+			} root->set_color(0); 
+			if (root->parent != NULL) {
+				cout << "çlkfasdf";
+			}
+        }
+
+        void RB_left_rotate (Node *ptr) {
+			Node* child = ptr->right; ptr->right = child->left;
+			if (ptr->right != NULL) {ptr->right->parent = ptr;}
+			child->parent = ptr->parent;
+			if (ptr->parent == NULL) {root = child;} else if (ptr == ptr->parent->left) {ptr->parent->left = child;} else {ptr->parent->right = child;}
+			child->left = ptr; ptr->parent = child;
+        }
+
+        void RB_right_rotate (Node *ptr) {
+			Node* child = ptr->left; ptr->left = child->right;
+			if (ptr->left != NULL) {ptr->left->parent = ptr;}
+			child->parent = ptr->parent;
+			if (ptr->parent == NULL) {root = child;} else if (ptr == ptr->parent->left) {ptr->parent->left = child;} else {ptr->parent->right = child;}
+			child->right = ptr; ptr->parent = child;
         }
 
         void preorder () {
 			if (root == nullptr) return; // Empty tree, nothing to traverse
-			
-			stack<Node*> stk;
-			stk.push(root);
-			
+			stack<pair<Node*, int>> stk;
+			stk.push({root, 0}); // Push root node and its depth
+
 			while (!stk.empty()) {
-				Node* node = stk.top();
-				stk.pop();
+				auto [current, depth] = stk.top(); stk.pop();
+
+				if (current == nullptr) continue;
+	
+				cout << depth << "(" << ((current->color == 1) ? "RED" : "BLACK") << ") " << current->key.name << endl; 
 				
-				// Process the current node
-				cout << string (stk.size(), '-') << node->key.name << endl;
-				
-				// Push the right child first so that the left child is processed next
-				if (node->right) stk.push(node->right);
-				if (node->left) stk.push(node->left);
+				if (current->right) {stk.push({current->right, depth + 1});}
+				if (current->left) {stk.push({current->left, depth + 1});}
 			}
+
 		}
 
         void find_best_seller () {
@@ -182,9 +182,8 @@ RB_tree generate_RBT_tree_from_csv (string file_name) {
 
 int main (int argc, char* argv[]) {
 	string fname = argv[1];	RB_tree RBtree = generate_RBT_tree_from_csv(fname);
-	
 	RBtree.preorder();
-    // Fill this function.
+	print_best_sellers(2020, RBtree.best_seller);
 
 	return EXIT_SUCCESS;
 }
