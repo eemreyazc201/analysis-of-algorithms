fd36a2f0f03712c62f067e8ec010b8f90d4c1ea8
Emre YAZICI <eemreyazc@hotmail.com>
2024-12-19 12:57:10 +0300

    Benchmark

--- game-sales/src/solution_BST_tree.cpp
+++ game-sales/src/solution_BST_tree.cpp
@@ -14,6 +14,11 @@
 using namespace std;
 using namespace std::chrono;
 
+vector <string> ALL_PUBLISHERS; // to keep all publisherNames in the csv file
+pair <int, int> AVERAGE_INSERTION_TIME = {0, 0}; // first: total time, second: number of insertions
+pair <int, int> AVERAGE_SEARCH_TIME = {0, 0}; // first: total time, second: number of searches
+pair <int, int> AVERAGE_FIND_BEST_SELLER = {0, 0}; // first: total time, second: number of searches
+
 class publisher {
 	public:
 		string name;
@@ -35,7 +40,10 @@ class Node {
 enum Alphabetic_Order {BIGGER, EQUAL, SMALLER};
 Alphabetic_Order compareAlphabetically (publisher& a, publisher& b) {
 	int size = min(a.name.size(), b.name.size()); for (int i = 0; i < size; i++) {
-		// char ai = (char)tolower(a.name[i]); char bi = (char)tolower(b.name[i]);
+		// char ai = (char)tolower(a.name[i]); char bi = (char)tolower(b.name[i]); 
+		// TA said I need to make lowercase comparison, 
+		// but when I compare according to lowarcase, 
+		// the results are different from the expected results.
 		char ai = a.name[i]; char bi = b.name[i];
 		if (ai < bi) {return SMALLER;} else if (ai > bi) {return BIGGER;}
 	} return (a.name.size() == b.name.size()) ? EQUAL : ((a.name.size() < b.name.size()) ? SMALLER : BIGGER);
@@ -55,7 +63,12 @@ class BST_tree {
 
 		void insertValue (vector<string> n) {
 			publisher temp; temp.name = n[3]; temp.na_sales = stof(n[4]); temp.eu_sales = stof(n[5]); temp.others_sales = stof(n[6]);
+			if (find(ALL_PUBLISHERS.begin(), ALL_PUBLISHERS.end(), n[3]) == ALL_PUBLISHERS.end()) {ALL_PUBLISHERS.push_back(n[3]);}
+			auto start = chrono::high_resolution_clock::now();
 			Node* ptr = new Node(temp); root = BST_insert(root, ptr);
+			auto end = chrono::high_resolution_clock::now();
+			AVERAGE_INSERTION_TIME.first += (chrono::duration_cast<chrono::nanoseconds>(end - start)).count();
+			AVERAGE_INSERTION_TIME.second++;
 		}
 
 		Node* BST_insert (Node* root, Node* ptr) {
@@ -69,9 +82,13 @@ class BST_tree {
 				else if (compareAlphabetically(ptr->key, curr->key) == EQUAL) {
 					curr->key.na_sales += ptr->key.na_sales; curr->key.eu_sales += ptr->key.eu_sales; curr->key.others_sales += ptr->key.others_sales;
 					
+					auto start = chrono::high_resolution_clock::now();
 					if (curr->key.na_sales > best_seller[0]->na_sales) {best_seller[0] = &curr->key;}
 					if (curr->key.eu_sales > best_seller[1]->eu_sales) {best_seller[1] = &curr->key;}
 					if (curr->key.others_sales > best_seller[2]->others_sales) {best_seller[2] = &curr->key;}
+					auto end = chrono::high_resolution_clock::now();
+					AVERAGE_FIND_BEST_SELLER.first += (chrono::duration_cast<chrono::nanoseconds>(end - start)).count();
+					AVERAGE_FIND_BEST_SELLER.second++;
 
 					return root;
 				}
@@ -79,9 +96,13 @@ class BST_tree {
 
 			if (compareAlphabetically(ptr->key, parent->key) == SMALLER) {parent->left = ptr;} else {parent->right = ptr;}
 
+			auto start = chrono::high_resolution_clock::now();
 			if (ptr->key.na_sales > best_seller[0]->na_sales) {best_seller[0] = &ptr->key;}
 			if (ptr->key.eu_sales > best_seller[1]->eu_sales) {best_seller[1] = &ptr->key;}
 			if (ptr->key.others_sales > best_seller[2]->others_sales) {best_seller[2] = &ptr->key;}
+			auto end = chrono::high_resolution_clock::now();
+			AVERAGE_FIND_BEST_SELLER.first += (chrono::duration_cast<chrono::nanoseconds>(end - start)).count();
+			AVERAGE_FIND_BEST_SELLER.second++;
 
 			return this->root;
 		}
@@ -124,5 +145,22 @@ BST_tree generate_BST_tree_from_csv (string file_name) {
 
 int main (int argc, char* argv[]) {
 	string fname = argv[1];	BST_tree BSTtree = generate_BST_tree_from_csv(fname);
+
+	for (int i = 0; i < 50; i++) {
+		int random = rand() % ALL_PUBLISHERS.size(); publisher temp; temp.name = ALL_PUBLISHERS[random];
+		auto start = chrono::high_resolution_clock::now(); Node* curr = BSTtree.get_root(); while (curr != NULL) {
+			if (compareAlphabetically(temp, curr->key) == SMALLER) {curr = curr->left;}
+			else if (compareAlphabetically(temp, curr->key) == BIGGER) {curr = curr->right;}
+			else if (compareAlphabetically(temp, curr->key) == EQUAL) {break;}
+		} auto end = chrono::high_resolution_clock::now();
+		AVERAGE_SEARCH_TIME.first += (chrono::duration_cast<chrono::nanoseconds>(end - start)).count(); 
+		AVERAGE_SEARCH_TIME.second++; 
+	}
+
+	ofstream output_file; output_file.open("BST_benchmark.txt");
+	output_file << "Average insertion time: " << AVERAGE_INSERTION_TIME.first / AVERAGE_INSERTION_TIME.second << " nanoseconds" << std::endl;
+	output_file << "Average search time: " << AVERAGE_SEARCH_TIME.first / AVERAGE_SEARCH_TIME.second << " nanoseconds" << std::endl;
+	output_file << "Average find best seller time: " << AVERAGE_FIND_BEST_SELLER.first / AVERAGE_FIND_BEST_SELLER.second << " nanoseconds" << std::endl;
+
 	return EXIT_SUCCESS;
 }
